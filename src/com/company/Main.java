6bd50e2586941b437075d2d583746efc2c95525a
francesco_genovasi <francesco.genovasi@studenti.unisalento.it>
2019-11-30 16:50:46 +0100

    test git

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,15 +4,5 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("hello world!");
-        System.out.println("aaa");
-        System.out.println("ccc");
-        System.out.println("bbb");
-        System.out.println("ddd");
-        System.out.println("eee");
-        System.out.println("fff");
-        System.out.println("ggg");
-        System.out.println("hhhh");
-        System.out.println("iiii");
-        System.out.println("eee");
     }
 }
