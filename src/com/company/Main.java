2de0e2aba5104397ce6eea39fe000adecc93de62
LordAssalt <alex.carluccio@studenti.unisalento.it>
2019-12-04 14:29:00 +0100

    test alex

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println("alex suca");
         System.out.println("hello world!");
     }
 }
