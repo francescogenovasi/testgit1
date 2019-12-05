e47b7f9e73a09955b07f74b6c53424fc77692f06
LordAssalt <alex.carluccio@studenti.unisalento.it>
2019-12-05 08:48:45 +0100

    test geno suca miiiiii

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("genovasi suca ghiei");
+        System.out.println("Barbagianni");
         System.out.println("hello world!");
     }
 }
